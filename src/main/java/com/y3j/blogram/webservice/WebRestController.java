7699390ee32d6e711103c1ed083a316ee31bb165
조성국 <jsk@joseong-gug-ui-MacBook-Pro.local>
2018-09-19 23:22:51 +0900

    Initial commit

---
+++ src/main/java/com/y3j/blogram/webservice/WebRestController.java
@@ -0,0 +1,13 @@
+package com.y3j.blogram.webservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+}
