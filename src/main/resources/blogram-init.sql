ca60fe0e543a5e720a1d0523ee8d0d1ab1e53b61
youngjun <june0313@gmail.com>
2018-10-21 20:53:57 +0900

    blogram-init.sql의 유저 권한부여 쿼리 수정

--- src/main/resources/blogram-init.sql
+++ src/main/resources/blogram-init.sql
@@ -5,5 +5,7 @@ CREATE DATABASE blogram;
 
 -- blogram user 생성
 CREATE USER 'blogramuser'@'localhost' IDENTIFIED BY 'blogrampass';
-GRANT ALL PRIVILEGES ON blogram.* To 'blogramuser'@'localhost' IDENTIFIED BY 'blogrampass';
+
+-- 권한부여
+GRANT ALL PRIVILEGES ON blogram.* To 'blogramuser'@'localhost';
 FLUSH PRIVILEGES;
