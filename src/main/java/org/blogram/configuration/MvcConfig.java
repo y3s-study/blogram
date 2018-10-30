2add9fbfce8c00bb1e63afee1c227c134abca67b
youngjun <june0313@gmail.com>
2018-10-30 23:25:58 +0900

    configuration 패키지 분리

--- src/main/java/org/blogram/configuration/MvcConfig.java
+++ src/main/java/org/blogram/configuration/MvcConfig.java
@@ -1,4 +1,4 @@
-package org.blogram.web;
+package org.blogram.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
--- src/main/java/org/blogram/configuration/WebSecurityConfig.java
+++ src/main/java/org/blogram/configuration/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package org.blogram.web;
+package org.blogram.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
