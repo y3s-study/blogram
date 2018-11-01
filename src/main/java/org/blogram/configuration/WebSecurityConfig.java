09585134fbe52544b20a111108585bfbdfbd41d7
youngjun <june0313@gmail.com>
2018-11-02 01:05:08 +0900

    핸들바 템플릿 공통 레이아웃 적용

--- src/main/java/org/blogram/configuration/WebSecurityConfig.java
+++ src/main/java/org/blogram/configuration/WebSecurityConfig.java
@@ -14,7 +14,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/index").permitAll()
+                .antMatchers("/", "/index", "/static/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
