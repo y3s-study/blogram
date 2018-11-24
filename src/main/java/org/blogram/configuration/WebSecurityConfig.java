0017cc610f35e81ae0788c0e9c3a73971337dd63
songseongbin <ppsosqq@gmail.com>
2018-11-24 17:13:54 +0900

    [#3] csrf 설정 disable

--- src/main/java/org/blogram/configuration/WebSecurityConfig.java
+++ src/main/java/org/blogram/configuration/WebSecurityConfig.java
@@ -14,7 +14,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/index", "/static/**").permitAll()
+                .antMatchers("/", "/index", "/static/**", "/join").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
@@ -23,7 +23,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .logout()
                 .logoutSuccessUrl("/")
-                .permitAll();
+                .permitAll()
+                .and()
+            .csrf().disable();
     }
 
     @Bean
