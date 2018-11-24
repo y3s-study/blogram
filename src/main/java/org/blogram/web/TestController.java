d39b6e151dcbfdadbb0552c001b49ea594a16eec
songseongbin <ppsosqq@gmail.com>
2018-11-24 17:29:09 +0900

    [#3] 회원가입 페이지 개발

--- src/main/java/org/blogram/web/TestController.java
+++ src/main/java/org/blogram/web/TestController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -23,5 +26,10 @@ public class TestController {
 		List<MemberDto> members = memberService.getAllMembers();
 		model.addAttribute("members", members);
 		return "test/memberList";
+	}
+
+	@GetMapping("/join")
+	public String join() {
+		return "/join";
 	}
 }
