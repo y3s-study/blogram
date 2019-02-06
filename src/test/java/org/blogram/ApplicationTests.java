c7d80c333175a002dc77014bcd8d5fefc7bfae10
songseongbin <ppsosqq@gmail.com>
2019-02-06 17:47:12 +0900

    [#21] Travis CI 설정 수정 및 @DataJpaTest 추가

--- src/test/java/org/blogram/ApplicationTests.java
+++ src/test/java/org/blogram/ApplicationTests.java
@@ -2,11 +2,13 @@ package org.blogram;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DataJpaTest
 public class ApplicationTests {
 
 	@Test
